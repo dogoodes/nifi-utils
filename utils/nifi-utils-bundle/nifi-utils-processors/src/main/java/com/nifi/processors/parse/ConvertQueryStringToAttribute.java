a48274f540425566c28733634db0dc5608b5f946
Macbook Air <fabianormatias@gmail.com>
2017-04-04 00:11:19 -0300

    finalizando o processor para converter query string to attribute

--- utils/nifi-utils-bundle/nifi-utils-processors/src/main/java/com/nifi/processors/parse/ConvertQueryStringToAttribute.java
+++ utils/nifi-utils-bundle/nifi-utils-processors/src/main/java/com/nifi/processors/parse/ConvertQueryStringToAttribute.java
@@ -1,6 +1,6 @@
 package com.nifi.processors.parse;
 
-import com.nifi.processors.utils.QueryParams;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.nifi.annotation.behavior.InputRequirement;
@@ -11,43 +11,23 @@ import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.logging.ComponentLog;
 import org.apache.nifi.processor.*;
-import org.apache.nifi.processor.io.InputStreamCallback;
 import org.apache.nifi.processor.util.StandardValidators;
-import org.apache.nifi.stream.io.StreamUtils;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 @Tags({"convert", "query string", "attribute"})
 @InputRequirement(Requirement.INPUT_ALLOWED)
 @CapabilityDescription("")
-public class ConvertQueryStringToMap extends AbstractProcessor {
+public class ConvertQueryStringToAttribute extends AbstractProcessor {
+
+    public static final String TRUE = "true";
 
     public static final PropertyDescriptor HTTP_REQUEST_URL = new PropertyDescriptor.Builder()
-		.name("Http Request URL")
-		.description("Http Request URL")
+		.name("Http request url")
+		.description("Http request url")
         .required(false)
         .dynamic(false)
         .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
@@ -57,7 +37,7 @@ public class ConvertQueryStringToMap extends AbstractProcessor {
 
     public static final PropertyDescriptor PREFIX = new PropertyDescriptor.Builder()
             .name("Prefix")
-            .description("Prefix")
+            .description("Prefix.")
             .required(false)
             .dynamic(false)
             .addValidator(StandardValidators.ATTRIBUTE_EXPRESSION_LANGUAGE_VALIDATOR)
@@ -66,7 +46,16 @@ public class ConvertQueryStringToMap extends AbstractProcessor {
 
     public static final PropertyDescriptor POSFIX = new PropertyDescriptor.Builder()
             .name("Posfix")
-            .description("Posfix")
+            .description("Posfix.")
+            .required(false)
+            .dynamic(false)
+            .addValidator(StandardValidators.ATTRIBUTE_EXPRESSION_LANGUAGE_VALIDATOR)
+            .expressionLanguageSupported(true)
+            .build();
+
+    public static final PropertyDescriptor ATTRIBUTE_VALUE_IF_EMPTY = new PropertyDescriptor.Builder()
+            .name("Attribute name if empty query string")
+            .description("Empty attribute if it doesn't have query string.")
             .required(false)
             .dynamic(false)
             .addValidator(StandardValidators.ATTRIBUTE_EXPRESSION_LANGUAGE_VALIDATOR)
@@ -90,6 +79,7 @@ public class ConvertQueryStringToMap extends AbstractProcessor {
         properties.add(HTTP_REQUEST_URL);
         properties.add(PREFIX);
         properties.add(POSFIX);
+        properties.add(ATTRIBUTE_VALUE_IF_EMPTY);
         this.properties = Collections.unmodifiableList(properties);
 
         final Set<Relationship> relationships = new HashSet<>();
@@ -124,34 +114,25 @@ public class ConvertQueryStringToMap extends AbstractProcessor {
         final String httpRequestUrl = context.getProperty(HTTP_REQUEST_URL).evaluateAttributeExpressions(flowFile).getValue();
         final String prefix = context.getProperty(PREFIX).evaluateAttributeExpressions(flowFile).getValue();
         final String posfix = context.getProperty(POSFIX).evaluateAttributeExpressions(flowFile).getValue();
+        final String attributeValueIfEmpty = context.getProperty(ATTRIBUTE_VALUE_IF_EMPTY).evaluateAttributeExpressions(flowFile).getValue();
 
+        List<NameValuePair> params = null;
         try {
-            List<NameValuePair> params = URLEncodedUtils.parse(new URI(httpRequestUrl), StandardCharsets.UTF_8);
+            logger.info("Fazendo o parse do url.");
+            params = URLEncodedUtils.parse(new URI(httpRequestUrl), StandardCharsets.UTF_8);
         } catch (URISyntaxException e) {
-            //TODO Acho que é necessário colocar o caminho de falha agora
             e.printStackTrace();
+            logger.error("Exception: " + e);
+            session.transfer(flowFile, FAILURE);
         }
 
+        if (!params.isEmpty())
+            for (NameValuePair param : params)
+                attributes.put(prefix + param.getName() + posfix, param.getValue());
+        else if (!StringUtils.isEmpty(attributeValueIfEmpty))
+            attributes.put(attributeValueIfEmpty, TRUE);
 
-//        final int lengthFlowFile = (int) flowFile.getSize();
-//        final byte[] buffer = new byte[lengthFlowFile];
-
-//        if (lengthFlowFile > 0) {
-//            final AtomicInteger bufferedByteCount = new AtomicInteger(0);
-//            session.read(flowFile, new InputStreamCallback() {
-//                @Override
-//                public void process(final InputStream in) throws IOException {
-//                    bufferedByteCount.set(StreamUtils.fillBuffer(in, buffer, false));
-//                }
-//            });
-//
-//            final String contentString = new String(buffer, 0, bufferedByteCount.get(), StandardCharsets.UTF_8);
-//
-//            flowFile = session.putAttribute(flowFile, attributeName, contentString);
-//        } else {
-//            flowFile = session.putAttribute(flowFile, attributeName, attributeValueIfEmpty);
-//        }
-
+        flowFile = session.putAllAttributes(flowFile, attributes);
         session.transfer(flowFile, SUCCESS);
     }
 
